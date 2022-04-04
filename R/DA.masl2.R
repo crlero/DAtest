0c754097f773c40d6eeb2e155e311f28e3b8bd69
Cristina Leal <cristina.lero@gmail.com>
2022-04-04 19:46:01 +0200

    update prunetests and description method

--- R/DA.masl2.R
+++ R/DA.masl2.R
@@ -1,4 +1,4 @@
-DA.masl = function (data, predictor, paired = NULL, covars = NULL, out.all = NULL, 
+DA.masl2 = function (data, predictor, paired = NULL, covars = NULL, out.all = NULL, 
           p.adj = "fdr", coeff = 2, coeff.ref = 1, allResults = FALSE, 
           ...) 
 {
