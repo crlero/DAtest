c32ab356d3c42f882ca1304f7abbf9f9300e25b5
Cristina Leal <cristina.lero@gmail.com>
2022-04-04 20:04:37 +0200

    add export fun

--- R/DA.masl.R
+++ R/DA.masl.R
@@ -1,3 +1,29 @@
+#' Maaslin2 with LM (no normalisation / no transformation)
+#'
+#' Implementation of DESeq2 for \code{DAtest}
+#' Manual geometric means calculated to avoid errors, see https://github.com/joey711/phyloseq/issues/387
+#' @param data Either a matrix with counts/abundances, OR a \code{phyloseq} object. If a matrix/data.frame is provided rows should be taxa/genes/proteins and columns samples
+#' @param predictor The predictor of interest. Either a Factor or Numeric, OR if \code{data} is a \code{phyloseq} object the name of the variable in \code{sample_data(data)} in quotation
+#' @param paired For paired/blocked experimental designs. Either a Factor with Subject/Block ID for running paired/blocked analysis, OR if \code{data} is a \code{phyloseq} object the name of the variable in \code{sample_data(data)} in quotation
+#' @param covars Either a named list with covariables, OR if \code{data} is a \code{phyloseq} object a character vector with names of the variables in \code{sample_data(data)}
+#' @param out.all If TRUE, will run "LRT" which will produce one p-value for the \code{predictor}. If FALSE will run "Wald" test and will output p-value from one level of the predictor specified by \code{coeff}. If NULL (default) set as TRUE for multi-class \code{predictor} and FALSE otherwise
+#' @param p.adj Character. P-value adjustment. Default "fdr". See \code{p.adjust} for details
+#' @param coeff Integer. The log2FoldChange (and p-value if test="Wald") will be associated with this coefficient. This coefficient is by default compared to the intercept (1. level of \code{predictor}), change this with \code{coeff.ref}. Default 2, i.e. the 2. level of the \code{predictor}.
+#' @param coeff.ref Integer. Reference level of the \code{predictor}. Default the intercept, = 1 
+#' @param allResults If TRUE will return raw results from the \code{DESeq} function
+#' @param ... Additional arguments for the \code{DESeq} function
+#' @return A data.frame with with results.
+#' @examples
+#' # Creating random count_table and predictor
+#' set.seed(4)
+#' mat <- matrix(rnbinom(200, size = 0.1, mu = 500), nrow = 20, ncol = 10)
+#' rownames(mat) <- 1:20
+#' pred <- c(rep("Control", 5), rep("Treatment", 5))
+#' 
+#' # Running masl
+#' res <- DA.masl(data = mat, predictor = pred)
+#' @export
+
 DA.masl = function (data, predictor, paired = NULL, covars = NULL, out.all = NULL, 
           p.adj = "fdr", coeff = 2, coeff.ref = 1, allResults = FALSE, 
           ...) 
@@ -17,7 +43,7 @@ DA.masl = function (data, predictor, paired = NULL, covars = NULL, out.all = NUL
     else {
       count_table <- data
     }
-    
+
     # normalize and transform data
     count_table <- apply(count_table, 2, function(x) {x/max(sum(x), 1e-32)}) # TSS normalization
     count_table <- apply(count_table, 2, function(x) log(x + min(x[x > 0])/2)) # LOG transformation (add pseudocount of min/2)
